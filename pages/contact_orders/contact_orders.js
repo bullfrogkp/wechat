e93c683b3adf629cabdc57eb56c943f183643722
Kevin Pan <pqhitpan@gmail.com>
2017-03-23 16:18:46 -0400

    mm

--- pages/contact_orders/contact_orders.js
+++ pages/contact_orders/contact_orders.js
@@ -41,9 +41,9 @@ Page({
       
   },
   // 跳转至详情页
-  contactDetail: function(e){
+  orderDetail: function(e){
     wx.navigateTo({
-      url:'../contact_detail/contact_detail?artype=' + e.currentTarget.dataset.artype
+      url:'../order_detail/order_detail?artype=' + e.currentTarget.dataset.artype
     })
   },
   // 加载更多
