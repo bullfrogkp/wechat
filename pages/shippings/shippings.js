526a0f3a881c3b515eb8bbbb6743325a96b9af21
Kevin Pan <pqhitpan@gmail.com>
2017-03-24 14:25:10 -0400

    mm

--- pages/shippings/shippings.js
+++ pages/shippings/shippings.js
@@ -41,15 +41,15 @@ Page({
       
   },
   // 跳转至详情页
-  addAgent: function(e){
+  addShipping: function(e){
     wx.navigateTo({
-      url:'../agent_detail/agent_detail?artype=' + e.currentTarget.dataset.artype
+      url:'../shipping_detail/shipping_detail?artype=' + e.currentTarget.dataset.artype
     })
   },
   // 跳转至详情页
-  agentDetail: function(e){
+  shippingDetail: function(e){
     wx.navigateTo({
-      url:'../agent_detail/agent_detail?artype=' + e.currentTarget.dataset.artype
+      url:'../shipping_detail/shipping_detail?artype=' + e.currentTarget.dataset.artype
     })
   },
   // 加载更多
